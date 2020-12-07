8741926cd4d1a18a3261f85dfe73bfeb9944ae51
One2Year <793150526@qq.com>
2020-12-07 17:46:14 +0800

    Upload front&back end files

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Vue_SpringBoot_Demo
 
-Login Page
+Learn from [Vue + Spring Boot 项目实战](https://blog.csdn.net/Neuf_Soleil/article/details/88925013)
+
+### Login Page
 [![Dxyf1g.png](https://s3.ax1x.com/2020/12/07/Dxyf1g.png)](https://imgchr.com/i/Dxyf1g)
 
-Success
+### Success
 [![DxyO9U.png](https://s3.ax1x.com/2020/12/07/DxyO9U.png)](https://imgchr.com/i/DxyO9U)
---
+++ vue-demo/src/components/Login.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        Username:<input type="text" v-model="loginForm.username" placeholder="Please enter your username"/>
+        <br><br>
+        Password: <input type="password" v-model="loginForm.password" placeholder="Please enter password"/>
+        <br><br>
+        <button v-on:click="login">Login</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    data () {
+        return {
+            loginForm: {
+                username: '',
+                password: ''
+            },
+            responseResult: []
+        }
+    },
+    methods: {
+        login () {
+            this.$axios
+                .post('/login', {
+                    username: this.loginForm.username,
+                    password: this.loginForm.password
+                })
+                .then(successResponse => {
+                    if (successResponse.data.code === 200) {
+                        this.$router.replace({path: '/index'})
+                    }
+                })
+                .catch(failResponse => {
+                    
+                })
+        }
+    }
+}
+</script>
